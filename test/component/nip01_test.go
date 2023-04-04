47ec1f5790df46c914dd1efcda2850b08bea2b9c
oso <opensourceoptimist@protonmail.com>
2023-04-04 20:51:40 +0200

    refactor test to one comp testfile per NIP

--- test/component/nip01_test.go
+++ test/component/nip01_test.go
@@ -2,15 +2,86 @@ package component
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/OpenSourceOptimist/skyflow/internal/event"
 	"github.com/OpenSourceOptimist/skyflow/internal/messages"
 	"github.com/OpenSourceOptimist/skyflow/internal/slice"
+	"github.com/google/uuid"
 	nostr "github.com/nbd-wtf/go-nostr"
 	"github.com/stretchr/testify/require"
+	"nhooyr.io/websocket"
 )
+
+func TestBasicNIP01Flow(t *testing.T) {
+	defer clearMongo()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, resp, err := websocket.Dial(ctx, "ws://localhost:80", nil)
+	require.NoError(t, err, "websocket dial error")
+	require.Equal(t, http.StatusSwitchingProtocols, resp.StatusCode, "handshake status")
+	defer conn.Close(websocket.StatusGoingAway, "bye")
+
+	testEvent := event.Event{
+		ID:        event.ID("d7dd5eb3ab747e16f8d0212d53032ea2a7cadef53837e5a6c66d42849fcb9027"),
+		Kind:      event.Kind(1),
+		PubKey:    event.PubKey("22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c"),
+		CreatedAt: event.Timestamp(1670869179),
+		Content:   "NOSTR \"WINE-ACCOUNT\" WITH HARVEST DATE STAMPED\n\n\n\"The older the wine, the greater its reputation\"\n\n\n22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c\n\n\nNWA 2022-12-12\nAA",
+		Tags:      []event.Tag{{"client", "astral"}},
+		Sig:       event.Signature("f110e4fdf67835fb07abc72469933c40bdc7334615610cade9554bf00945a1cebf84f8d079ec325d26fefd76fe51cb589bdbe208ac9cdbd63351ddad24a57559"),
+	}
+
+	reqBytes, err := json.Marshal([]interface{}{"EVENT", testEvent})
+	require.NoError(t, err)
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, reqBytes))
+
+	subscriptionID := uuid.NewString()
+	reqBytes, err = json.Marshal([]interface{}{"REQ", subscriptionID, messages.Filter{IDs: []event.ID{testEvent.ID}}})
+	require.NoError(t, err)
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, reqBytes))
+
+	msgType, responseBytes, err := conn.Read(ctx)
+	require.NoError(t, err)
+	require.Equal(t, websocket.MessageText, msgType)
+
+	var eventDataMsg []json.RawMessage
+	require.NoError(t, json.Unmarshal(responseBytes, &eventDataMsg))
+	require.Len(t, eventDataMsg, 3)
+	var recivedMsgType string
+	require.NoError(t, json.Unmarshal(eventDataMsg[0], &recivedMsgType))
+	require.Equal(t, "EVENT", recivedMsgType)
+}
+
+var validEvent = event.Event{
+	ID:        event.ID("d7dd5eb3ab747e16f8d0212d53032ea2a7cadef53837e5a6c66d42849fcb9027"),
+	Kind:      event.Kind(1),
+	PubKey:    event.PubKey("22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c"),
+	CreatedAt: event.Timestamp(1670869179),
+	Content:   "NOSTR \"WINE-ACCOUNT\" WITH HARVEST DATE STAMPED\n\n\n\"The older the wine, the greater its reputation\"\n\n\n22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c\n\n\nNWA 2022-12-12\nAA",
+	Tags:      []event.Tag{{"client", "astral"}},
+	Sig:       event.Signature("f110e4fdf67835fb07abc72469933c40bdc7334615610cade9554bf00945a1cebf84f8d079ec325d26fefd76fe51cb589bdbe208ac9cdbd63351ddad24a57559"),
+}
+
+func TestClosing(t *testing.T) {
+	defer clearMongo()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	conn, closer := newSocket(ctx, t)
+	defer closer()
+	subID1 := requestSub(ctx, t, conn, messages.Filter{})
+	cancelSub(ctx, t, subID1, conn)
+	publish(ctx, t, validEvent, conn)
+	subID2 := requestSub(ctx, t, conn, messages.Filter{})
+	sub, e, err := readEvent(ctx, t, conn)
+	require.NoError(t, err)
+	require.Equal(t, subID2, sub)
+	require.Equal(t, validEvent.ID, e.ID)
+}
 
 func TestBasicFiltering(t *testing.T) {
 
@@ -217,4 +288,92 @@ func TestMoreComplicatedFiltering(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGetEventsAfterInitialSync(t *testing.T) {
+	priv, pub := NewKeyPair(t)
+	conn1 := NewConnection(t)
+	ctx := context.Background()
+	initialPublishedEvent := NewSignedEvent(t, EventOptions{PrivKey: priv, Content: "hello world"})
+	status := conn1.Publish(ctx, initialPublishedEvent)
+	require.Equal(t, nostr.PublishStatusSucceeded, status)
+
+	conn2 := NewConnection(t)
+	subscription := conn2.Subscribe(ctx, nostr.Filters{{Authors: []string{string(pub)}}})
+	select {
+	case initialRecivedEvent := <-subscription.Events:
+		require.Equal(t, toEvent(initialPublishedEvent), toEvent(*initialRecivedEvent))
+	case <-time.After(time.Second):
+		require.Fail(t, "timed out waiting for initial event to be recived")
+	}
+
+	secondPublishedEvent := NewSignedEvent(t, EventOptions{PrivKey: priv, Content: "hello again"})
+	require.Equal(t,
+		conn1.Publish(ctx, secondPublishedEvent),
+		nostr.PublishStatusSucceeded,
+	)
+
+	select {
+	case secondRecivedEvent := <-subscription.Events:
+		require.Equal(t, toEvent(secondPublishedEvent), toEvent(*secondRecivedEvent))
+	case <-time.After(time.Second):
+		require.Fail(t, "timed out waiting for second event to be recived")
+	}
+}
+
+func TestVerificationEventID(t *testing.T) {
+	defer clearMongo()
+	ctx := context.Background()
+	conn, _, _ := websocket.Dial(ctx, "ws://localhost:80", nil)
+	defer conn.Close(websocket.StatusGoingAway, "bye")
+	testEvent := event.Event{
+		ID:        event.ID("d8dd5eb23b747e16f8d0212d53032ea2a7cadef53837e5a6c66142843fcb9027"), // modified in a couple of places
+		Kind:      event.Kind(1),
+		PubKey:    event.PubKey("22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c"),
+		CreatedAt: event.Timestamp(1670869179),
+		Content:   "NOSTR \"WINE-ACCOUNT\" WITH HARVEST DATE STAMPED\n\n\n\"The older the wine, the greater its reputation\"\n\n\n22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c\n\n\nNWA 2022-12-12\nAA",
+		Tags:      []event.Tag{{"client", "astral"}},
+		Sig:       event.Signature("f110e4fdf67835fb07abc72469933c40bdc7334615610cade9554bf00945a1cebf84f8d079ec325d26fefd76fe51cb589bdbe208ac9cdbd63351ddad24a57559"),
+	}
+
+	// Writing a bad event should work
+	eventMsg, _ := json.Marshal([]any{"EVENT", testEvent})
+
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, eventMsg))
+	// But should not show up in a request
+	reqBytes, _ := json.Marshal([]interface{}{"REQ", uuid.NewString(), messages.Filter{IDs: []event.ID{testEvent.ID}}})
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, reqBytes))
+	ctx, cancelFunc := context.WithTimeout(ctx, time.Second)
+	defer cancelFunc()
+	_, readBytes, err := conn.Read(ctx)
+	require.Equal(t, "", string(readBytes))
+	require.ErrorIs(t, err, context.DeadlineExceeded)
+}
+
+func TestVerificationSignature(t *testing.T) {
+	defer clearMongo()
+	ctx := context.Background()
+	conn, _, _ := websocket.Dial(ctx, "ws://localhost:80", nil)
+	defer conn.Close(websocket.StatusGoingAway, "bye")
+	testEvent := event.Event{
+		ID:        event.ID("d7dd5eb3ab747e16f8d0212d53032ea2a7cadef53837e5a6c66d42849fcb9027"),
+		Kind:      event.Kind(1),
+		PubKey:    event.PubKey("22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c"),
+		CreatedAt: event.Timestamp(1670869179),
+		Content:   "NOSTR \"WINE-ACCOUNT\" WITH HARVEST DATE STAMPED\n\n\n\"The older the wine, the greater its reputation\"\n\n\n22a12a128a3be27cd7fb250cbe796e692896398dc1440ae3fa567812c8107c1c\n\n\nNWA 2022-12-12\nAA",
+		Tags:      []event.Tag{{"client", "astral"}},
+		Sig:       event.Signature("f111e4fdf67835fb07abc72469933c40bdc7334615610cade9554bf00945a1cebf84f8d079ec325d26fefd76fe51cb589bdbe208ac9cdbd63351ddad24a57559"), // modified
+	}
+
+	// Writing a bad event should work
+	eventMsg, _ := json.Marshal([]any{"EVENT", testEvent})
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, eventMsg))
+	// But should not show up in a request
+	reqBytes, _ := json.Marshal([]interface{}{"REQ", uuid.NewString(), messages.Filter{IDs: []event.ID{testEvent.ID}}})
+	require.NoError(t, conn.Write(ctx, websocket.MessageText, reqBytes))
+	ctx, cancelFunc := context.WithTimeout(ctx, time.Second)
+	defer cancelFunc()
+	_, readBytes, err := conn.Read(ctx)
+	require.Equal(t, "", string(readBytes))
+	require.ErrorIs(t, err, context.DeadlineExceeded)
 }
